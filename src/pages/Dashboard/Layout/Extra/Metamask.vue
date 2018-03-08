2b37f8b55ff68d65563060643273310c68f92612
shanerich <shanerich@gmail.com>
2018-03-08 10:26:12 -0500

    Initial commit

---
+++ src/pages/Dashboard/Layout/Extra/Metamask.vue
@@ -0,0 +1,20 @@
+<template>
+    <span>{{ web3.coinbase | sliceNeg7 }}</span>
+</template>
+<script>
+export default {
+    name: 'metamask',
+    computed: {
+        web3 () {
+            return this.$store.state.web3
+        },
+    },
+    filters: {
+        sliceNeg7: function (value) {
+            return (!value ? '' : value.slice(-7))
+        }
+    }
+}
+</script>
+<style scoped>
+</style>
---
+++ src/pages/Dashboard/Layout/Extra/UserMenu.vue
@@ -0,0 +1,78 @@
+<template>
+  <div class="user">
+    <div class="photo">
+      <img src="static/img/metamask-avatar.png" alt="avatar"/>
+    </div>
+    <div class="info">
+      <a data-toggle="collapse" :aria-expanded="!isClosed" @click.stop="toggleMenu" href="#">
+           <span>
+             <metamask/>
+             <b class="caret"></b>
+          </span>
+      </a>
+      <div class="clearfix"></div>
+      <div>
+        <el-collapse-transition>
+          <ul class="nav" v-show="!isClosed">
+            <slot>
+              <li>
+                <a class="profile-dropdown" href="#pablo">
+                  <span class="sidebar-mini">MP</span>
+                  <span class="sidebar-normal">My Profile</span>
+                </a>
+              </li>
+              <li>
+                <a class="profile-dropdown" href="#pablo">
+                  <span class="sidebar-mini">EP</span>
+                  <span class="sidebar-normal">Edit Profile</span>
+                </a>
+              </li>
+              <li>
+                <a class="profile-dropdown" href="#pablo">
+                  <span class="sidebar-mini">S</span>
+                  <span class="sidebar-normal">Settings</span>
+                </a>
+              </li>
+            </slot>
+          </ul>
+        </el-collapse-transition>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  import CollapseTransition from 'element-ui/lib/transitions/collapse-transition'
+  import Metamask from 'src/pages/Dashboard/Layout/Extra/Metamask'
+  
+  export default {
+    beforeCreate () {
+      console.log('registerWeb3 Action dispatched')
+      this.$store.dispatch('registerWeb3')
+    },
+    components: {
+      [CollapseTransition.name]: CollapseTransition,
+      'Metamask': Metamask
+    },
+    props: {
+      title: {
+        type: String,
+        default: 'Tania Andrew'
+      }
+    },
+    data () {
+      return {
+        isClosed: true
+      }
+    },
+    methods: {
+      toggleMenu () {
+        this.isClosed = !this.isClosed
+      }
+    }
+  }
+</script>
+<style>
+  .collapsed {
+    transition: opacity 1s;
+  }
+</style>
